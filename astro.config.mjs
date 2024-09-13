03e0f8d15c469cd77c84523ddc7ff007415249b4
mstermigol <migueljaramillo879@gmail.com>
2024-09-13 01:13:02 -0500

    Fix navbar, add dismiss to collapse, add slot fot full width

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import colors from 'tailwindcss/colors';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind({
+    applyBaseStyles: true,
+  })]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@
 import colors from 'tailwindcss/colors';
 export default {
   content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}",
     "./node_modules/preline/preline.js",
   ],
   theme: {
@@ -10,9 +10,13 @@ export default {
       colors: {
         background: "#f1f1f1",
         blue: "#1993c5",
-		'light-blue': "#60bad4",
+		    'light-blue': "#60bad4",
 		neutral: colors.neutral
       },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        lalezar: ['Lalezar', 'cursive'],
+      }
     },
   },
   plugins: [
