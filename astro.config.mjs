ed001e0da3dbd3d795adbacaaea1d0794a017997
mstermigol <migueljaramillo879@gmail.com>
2024-09-18 22:46:30 -0500

    Refactor hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   integrations: [tailwind({
   applyBaseStyles: true,
-})]
+}), icon()]
 });
