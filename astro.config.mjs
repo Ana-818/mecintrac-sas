abd2c110c5db07037052490365823552a42348cd
Miguel Jaramillo Arenas <migueljaramillo@gmail.com>
2025-02-28 22:38:21 -0500

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 
 import icon from 'astro-icon';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://mecintrac.com',
   integrations: [tailwind({
   applyBaseStyles: true,
-}), icon(), mdx()]
+}), icon(), mdx(), robotsTxt(), sitemap()]
 });
